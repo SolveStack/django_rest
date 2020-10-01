67760318050629db22a30a8e8b5652e5ac42d028
bkdevart <bkdevart@gmail.com>
2020-10-01 16:05:10 -0700

    rebuilt db, updated notes, added in viewsand serializers for user tracking

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,7 +1,8 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
-
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -11,3 +12,12 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
