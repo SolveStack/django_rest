540fb06ead597b5345c0d9021d9eec0c809e1672
bkdevart <bkdevart@gmail.com>
2020-10-03 07:16:29 -0700

    finished tutorial

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -41,9 +41,3 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
